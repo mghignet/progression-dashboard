9e7d9e1172de7b7998e85322152f3d104b070107
Thibaut STRECKER <thibaut.strecker@ext.leroymerlin.fr>
2022-02-05 15:33:14 +0100

    chore: upgrade vue dependencies and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,9 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
   overrides: [
     {
